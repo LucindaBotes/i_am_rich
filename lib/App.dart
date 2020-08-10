482dad08661ae59f1bcfafd4d1b0d88eee0ec7a2
LucindaBotes <lucinda.b2000@gmail.com>
2020-08-10 09:48:42 +0200

    Create App Icon

---
+++ lib/App.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('This is text'),
+      color: (Colors.pinkAccent[50]),
+    );
+  }
+}
